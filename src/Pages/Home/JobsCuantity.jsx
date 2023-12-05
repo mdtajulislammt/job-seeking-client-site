0ab7989a50f8172819e6b715b31dffd44863f915
MD Tajul Islam Mt <62564047+mdtajulislammt@users.noreply.github.com>
2023-12-05 17:54:27 +0600

    Update JobsCuantity.jsx

--- src/Pages/Home/JobsCuantity.jsx
+++ src/Pages/Home/JobsCuantity.jsx
@@ -1,7 +1,7 @@
 
 const JobsCuantity = () => {
      return (
-          <div className="bg-[#3994e4] flex justify-between p-10 rounded px-10 text-center pb-5 lg:mx-20 md:mx-16 mx-10 ">
+          <div className="bg-[#3994e4] md:flex my-10 justify-between p-10 rounded px-10 text-center pb-5 lg:mx-20 md:mx-16 mx-10 ">
               <div>
                <h2 className=" text-white text-5xl  font-bold">2540 +</h2>
                <div className=" w-14 my-5 mx-auto h-1 bg-white"></div>
